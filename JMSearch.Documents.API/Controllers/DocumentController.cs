6aed653c477d346fdfea475839282c8e397a5e34
Tonef <d.maignan@iia-laval.fr>
2018-03-30 15:06:52 +0200

    Documents.API : Ajout du Controleur pour récupérer les PDF

---
+++ JMSearch.Documents.API/Controllers/DocumentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.IO;
+
+namespace JMSearch.Documents.API.Controllers
+{
+	/// <summary>
+	/// Contrôleur pour les documents
+	/// </summary>
+	[Route("api/Documents")]
+	public class DocumentController : Controller
+    {
+		private readonly string _pathDocuments = @"C:\Temp\Documents";
+
+		/// <summary>
+		/// Obtient un pdf en fonction du nom.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		[Produces(typeof(Stream))]
+		public IActionResult Get(string name)
+		{
+			if (!Directory.Exists(_pathDocuments))
+				throw new DirectoryNotFoundException("Le dossier : \"" + _pathDocuments + "\" n'existe pas.");
+
+			var fileInfo = new FileInfo(Path.Combine(_pathDocuments, name + ".pdf"));
+
+			if (!fileInfo.Exists)
+				return BadRequest();
+
+			return Ok(fileInfo.OpenRead());
+		}
+    }
+}
