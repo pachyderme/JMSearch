9ad7e6602f7c0a56ff135cee45f2be8ef62c5ab2
pachyderme <bouvetpierre49@gmail.com>
2018-03-30 12:24:40 +0200

    Swagger

--- JMSearch.API/Startup.cs
+++ JMSearch.API/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace JMSearch.API
 {
@@ -24,6 +20,19 @@ namespace JMSearch.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "JMSearch API",
+                    Description = "TODO.\n",
+                    Contact = new Contact { Name = "JMSearch", Email = "contact@jmsearch.com", Url = "" }
+                });
+                var filePath = System.IO.Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "JMSearch.API.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +44,17 @@ namespace JMSearch.API
             }
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JMSearch API V1");
+                c.RoutePrefix = "doc";
+                c.DocumentTitle = "JMSearch API Doc";
+            });
         }
     }
 }
