3fd4fd52422f144e43fc738ec794db0f9ad2c4c8
pachyderme <bouvetpierre49@gmail.com>
2018-03-30 13:50:02 +0200

    Docments.API project and swagger

---
+++ JMSearch.Documents.API/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace JMSearch.Documents.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "JMSearch.Documents API",
+                    Description = "TODO.\n",
+                    Contact = new Contact { Name = "JMSearch.Documents", Email = "contact@jmsearch-documents.com", Url = "" }
+                });
+                var filePath = System.IO.Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "JMSearch.Documents.API.xml");
+                c.IncludeXmlComments(filePath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JMSearch.Documents API V1");
+                c.RoutePrefix = "doc";
+                c.DocumentTitle = "JMSearch.Documents API Doc";
+            });
+        }
+    }
+}
