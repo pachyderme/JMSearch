236e43a5e06d640c3c506e279dd40f391fe9f696
pachyderme <bouvetpierre49@gmail.com>
2018-03-30 14:04:03 +0200

    SearchController and FileController (base)

---
+++ JMSearch.API/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JMSearch.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Search")]
+    public class SearchController : Controller
+    {
+        /// <summary>
+        /// GET the responses from a keyword
+        /// </summary>
+        /// <param name="keyWord"></param>
+        /// <returns></returns>
+        [HttpGet("GetResponses/{keyWord}")]
+        public string GetResponses(string keyWord)
+        {
+            return "good";
+        }
+    }
+}
