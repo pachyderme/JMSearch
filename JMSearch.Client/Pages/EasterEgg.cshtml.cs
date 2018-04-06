e05095b77a435766caeeeb04bf6a74bb5edf3817
pachyderme <bouvetpierre49@gmail.com>
2018-04-06 12:19:32 +0200

    easter egg

---
+++ JMSearch.Client/Pages/EasterEgg.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace JMSearch.Client.Pages
+{
+    public class EasterEggModel : PageBase
+    {
+        public EasterEggModel(IMemoryCache memoryCache) : base(memoryCache)
+        {
+        }
+    }
+}
